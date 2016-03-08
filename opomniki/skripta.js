033486037309a6911dc124e18438c05fa1fab0f9
Matevž Fabjančič <mf6422@student.uni-lj.si>
2016-03-08 15:34:07 +0000

    Stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -7,7 +7,7 @@ window.addEventListener('load', function() {
 		var uporabnik = document.querySelector("#uporabnisko_ime").value;
 		document.querySelector("#uporabnik").innerHTML = uporabnik;
 		document.querySelector(".pokrivalo").style.visibility = "hidden";
-	}
+	};
 	
 	var novOpomnik = function(event) {
 		
@@ -18,7 +18,7 @@ window.addEventListener('load', function() {
 		document.querySelector("#cas_opomnika").value = '';
 		
 		document.querySelector("#opomniki").innerHTML += "<div class='opomnik senca rob'><div class='naziv_opomnika'>" + nazivZadolzitve + "</div><div class='cas_opomnika'>Opomnik čez <span>" + cas + "</span> sekund.</div></div>";
-	}
+	};
 	
 	
 	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
@@ -37,8 +37,15 @@ window.addEventListener('load', function() {
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
+			if (cas == 0) {
+				var naziv_opomnika = opomnik.querySelector(".naziv_opomnika").innerHTML;
+				document.querySelector("#opomniki").removeChild(opomnik);
+				alert("Opomnik!\n\nZadolžitev '" + naziv_opomnika + "' je potekla!");
+			} else {
+				casovnik.innerHTML = cas - 1;
+			}
 		}
-	}
+	};
 	setInterval(posodobiOpomnike, 1000);
 	
 });
